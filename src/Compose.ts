d652473c3e0372f24b2c21ef1a5819dac142bf35
Juyeong Maing <mjy9088@naver.com>
2025-10-07 09:43:27 +0000

    fix compose

--- src/Compose.ts
+++ src/Compose.ts
@@ -1,28 +1,9 @@
 import { Pipe } from './Pipe';
 import { Transformer } from './Transformer';
 import { IfHasRaw } from './util/IfHasRaw';
-import { IsNever } from './util/IsNever';
 import { RawField } from './util/RawField';
 
-export interface Compose2<
-  K1 extends Transformer<unknown, unknown>,
-  K2 extends Transformer<K1['output'], unknown>,
-> extends Transformer<K1['input'], K2['output']> {
-  output: IfHasRaw<
-    this,
-    'input',
-    Compose2Raw<K1, K2, RawField<this, 'input'>>,
-    K2['output']
-  >;
-}
-
-export type Compose2Raw<
-  K1 extends Transformer<unknown, unknown>,
-  K2 extends Transformer<K1['output'], unknown>,
-  Input extends K1['input'],
-> = Pipe<Input, K1, K2>;
-
-export type Compose<
+export interface Compose<
   K1 extends Transformer<unknown, unknown>,
   K2 extends Transformer<K1['output'], unknown> = never,
   K3 extends Transformer<K2['output'], unknown> = never,
@@ -49,67 +30,71 @@ export type Compose<
   K24 extends Transformer<K23['output'], unknown> = never,
   K25 extends Transformer<K24['output'], unknown> = never,
   K26 extends Transformer<K25['output'], unknown> = never,
-> = IsNever<K26> extends false
-  ? // prettier-ignore
-    Compose2<Compose<K1, K2, K3, K4, K5, K6, K7, K8, K9, K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K20, K21, K22, K23, K24, K25>, K26>
-  : IsNever<K25> extends false
-  ? // prettier-ignore
-    Compose2<Compose<K1, K2, K3, K4, K5, K6, K7, K8, K9, K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K20, K21, K22, K23, K24>, K25>
-  : IsNever<K24> extends false
-  ? // prettier-ignore
-    Compose2<Compose<K1, K2, K3, K4, K5, K6, K7, K8, K9, K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K20, K21, K22, K23>, K24>
-  : IsNever<K23> extends false
-  ? // prettier-ignore
-    Compose2<Compose<K1, K2, K3, K4, K5, K6, K7, K8, K9, K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K20, K21, K22>, K23>
-  : IsNever<K22> extends false
-  ? // prettier-ignore
-    Compose2<Compose<K1, K2, K3, K4, K5, K6, K7, K8, K9, K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K20, K21>, K22>
-  : IsNever<K21> extends false
-  ? // prettier-ignore
-    Compose2<Compose<K1, K2, K3, K4, K5, K6, K7, K8, K9, K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K20>, K21>
-  : IsNever<K20> extends false
-  ? // prettier-ignore
-    Compose2<Compose<K1, K2, K3, K4, K5, K6, K7, K8, K9, K10, K11, K12, K13, K14, K15, K16, K17, K18, K19>, K20>
-  : IsNever<K19> extends false
-  ? // prettier-ignore
-    Compose2<Compose<K1, K2, K3, K4, K5, K6, K7, K8, K9, K10, K11, K12, K13, K14, K15, K16, K17, K18>, K19>
-  : IsNever<K18> extends false
-  ? // prettier-ignore
-    Compose2<Compose<K1, K2, K3, K4, K5, K6, K7, K8, K9, K10, K11, K12, K13, K14, K15, K16, K17>, K18>
-  : IsNever<K17> extends false
-  ? // prettier-ignore
-    Compose2<Compose<K1, K2, K3, K4, K5, K6, K7, K8, K9, K10, K11, K12, K13, K14, K15, K16>, K17>
-  : IsNever<K16> extends false
-  ? // prettier-ignore
-    Compose2<Compose<K1, K2, K3, K4, K5, K6, K7, K8, K9, K10, K11, K12, K13, K14, K15>, K16>
-  : IsNever<K15> extends false
-  ? // prettier-ignore
-    Compose2<Compose<K1, K2, K3, K4, K5, K6, K7, K8, K9, K10, K11, K12, K13, K14>, K15>
-  : IsNever<K14> extends false
-  ? // prettier-ignore
-    Compose2<Compose<K1, K2, K3, K4, K5, K6, K7, K8, K9, K10, K11, K12, K13>, K14>
-  : IsNever<K13> extends false
-  ? Compose2<Compose<K1, K2, K3, K4, K5, K6, K7, K8, K9, K10, K11, K12>, K13>
-  : IsNever<K12> extends false
-  ? Compose2<Compose<K1, K2, K3, K4, K5, K6, K7, K8, K9, K10, K11>, K12>
-  : IsNever<K11> extends false
-  ? Compose2<Compose<K1, K2, K3, K4, K5, K6, K7, K8, K9, K10>, K11>
-  : IsNever<K10> extends false
-  ? Compose2<Compose<K1, K2, K3, K4, K5, K6, K7, K8, K9>, K10>
-  : IsNever<K9> extends false
-  ? Compose2<Compose<K1, K2, K3, K4, K5, K6, K7, K8>, K9>
-  : IsNever<K8> extends false
-  ? Compose2<Compose<K1, K2, K3, K4, K5, K6, K7>, K8>
-  : IsNever<K7> extends false
-  ? Compose2<Compose<K1, K2, K3, K4, K5, K6>, K7>
-  : IsNever<K6> extends false
-  ? Compose2<Compose<K1, K2, K3, K4, K5>, K6>
-  : IsNever<K5> extends false
-  ? Compose2<Compose<K1, K2, K3, K4>, K5>
-  : IsNever<K4> extends false
-  ? Compose2<Compose<K1, K2, K3>, K4>
-  : IsNever<K3> extends false
-  ? Compose2<Compose2<K1, K2>, K3>
-  : IsNever<K2> extends false
-  ? Compose2<K1, K2>
-  : K1;
+  K27 extends Transformer<K26['output'], unknown> = never,
+  K28 extends Transformer<K27['output'], unknown> = never,
+  K29 extends Transformer<K28['output'], unknown> = never,
+  K30 extends Transformer<K29['output'], unknown> = never,
+  K31 extends Transformer<K30['output'], unknown> = never,
+  K32 extends Transformer<K31['output'], unknown> = never,
+  K33 extends Transformer<K32['output'], unknown> = never,
+  K34 extends Transformer<K33['output'], unknown> = never,
+  K35 extends Transformer<K34['output'], unknown> = never,
+  K36 extends Transformer<K35['output'], unknown> = never,
+  K37 extends Transformer<K36['output'], unknown> = never,
+  K38 extends Transformer<K37['output'], unknown> = never,
+  K39 extends Transformer<K38['output'], unknown> = never,
+  K40 extends Transformer<K39['output'], unknown> = never,
+  K41 extends Transformer<K40['output'], unknown> = never,
+  K42 extends Transformer<K41['output'], unknown> = never,
+> extends Transformer<K1['input'], K2['output']> {
+  output: IfHasRaw<
+    this,
+    'input',
+    Pipe<
+      RawField<this, 'input'>,
+      K1,
+      K2,
+      K3,
+      K4,
+      K5,
+      K6,
+      K7,
+      K8,
+      K9,
+      K10,
+      K11,
+      K12,
+      K13,
+      K14,
+      K15,
+      K16,
+      K17,
+      K18,
+      K19,
+      K20,
+      K21,
+      K22,
+      K23,
+      K24,
+      K25,
+      K26,
+      K27,
+      K28,
+      K29,
+      K30,
+      K31,
+      K32,
+      K33,
+      K34,
+      K35,
+      K36,
+      K37,
+      K38,
+      K39,
+      K40,
+      K41,
+      K42
+    >,
+    K2['output']
+  >;
+}
